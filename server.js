039d2ff85dbae025dd380c1f8a7b7c7374f176ff
Nick Roney <nroney21@gmail.com>
2016-07-14 23:18:55 -0400

    Started connecting the dots with both apis

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
 var Yelp = require('yelp');
+var _ = require('lodash');
 
 var http = require('http');
 var parser = require('xml2json');
@@ -10,12 +11,10 @@ var rest = require('restler');
 var crypto = require('crypto');
 var apiKey = '023eaa572e7f49b4be6415308282d45d';
 var fatSecretRestUrl = 'http://platform.fatsecret.com/rest/server.api';
-var sharedSecret = '4970d55d2b8d408886a566b86cb984bd';
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 
 var port = process.env.PORT || 8081;
 var router = express.Router();
@@ -25,8 +24,9 @@ router.get('/', function (req, res) {
 });
 
 router.get('/getFood', function (req, res) {
+    var sharedSecret = '4970d55d2b8d408886a566b86cb984bd';
     var newDate = new Date();
-    var searchTerm = req.query.food;
+    var searchTerm = encodeURIComponent(req.query.food);
     var reqObj = {
         method: 'foods.search',
         oauth_consumer_key: apiKey,
@@ -37,13 +37,11 @@ router.get('/getFood', function (req, res) {
         search_expression: searchTerm
     };
 
-    // construct a param=value& string and uriEncode
     var paramsStr = '';
     for (var i in reqObj) {
         paramsStr += "&" + i + "=" + reqObj[i];
     }
 
-// yank off that first "&"
     paramsStr = paramsStr.substr(1);
 
     var sigBaseStr = "POST&"
@@ -51,29 +49,23 @@ router.get('/getFood', function (req, res) {
         + "&"
         + encodeURIComponent(paramsStr);
 
-// no  Access Token token (there's no user .. we're just calling foods.search)
     sharedSecret += "&";
 
-    var hashedBaseStr  = crypto.createHmac('sha1', sharedSecret).update(sigBaseStr).digest('base64');
+    var hashedBaseStr = crypto.createHmac('sha1', sharedSecret).update(sigBaseStr).digest('base64');
 
-// Add oauth_signature to the request object
     reqObj.oauth_signature = hashedBaseStr;
     request({
-        url: fatSecretRestUrl, //URL to hit
+        url: fatSecretRestUrl,
         method: 'POST',
-        qs: reqObj
+        qs: reqObj,
+        jar: false
     }, function (error, response, body) {
         if (error) {
             console.log(error);
         } else {
             var xmlData = body;
-
             var convertedData = JSON.parse(parser.toJson(xmlData));
             res.json({message: convertedData});
-            console.log('oauth_timestamp:'+reqObj.oauth_timestamp);
-            console.log('oauth_nonce:'+reqObj.oauth_nonce);
-            console.log('hashedBaseStr:'+hashedBaseStr);
-            console.log('sigBaseStr:'+ sigBaseStr)
         }
     });
 });
@@ -88,10 +80,18 @@ router.get('/getLocations', function (req, res) {
 
     var radius = (1609.34 * parseInt(req.query.radius_filter));
 
+    var locations = [];
+
     yelp.search({term: 'restaurant', ll: req.query.lat + ',' + req.query.lon, radius_filter: radius})
         .then(function (data) {
             res.json({message: data});
+            return data;
+        }).then(function (data) {
+        _.forEach(data.businesses, function(value, key) {
+            locations.push(value.name);
         });
+        var l = '';
+    })
 });
 
 router.get('/heartbeat', function (req, res) {
